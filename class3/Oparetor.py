b662c966432aaf969b9127b9f2bd2477619ab73a
Shakhawat Hossain <shakhawat.sk@gmail.com>
2025-02-27 00:31:12 +0600

    Oparators

---
+++ class3/Oparetor.py
@@ -0,0 +1,16 @@
+a = 10
+b = 3
+print("")
+print(a + b)   # 13
+print(a - b)   # 7
+print(a * b)   # 30
+print(a / b)   # 3.3333
+print(a // b)  # 3
+print(a % b)   # 1
+print(a ** b)  # 1000
+
+abc = "ABC"
+
+abc += "123"
+
+print(abc)
